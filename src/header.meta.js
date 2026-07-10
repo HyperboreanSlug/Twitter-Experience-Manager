390d7ca4fe0414c9bdb3fd82e603d7a0d8b1fffe
HyperboreanSlug <hyperboreanslug@users.noreply.github.com>
2026-07-10 00:39:22 -0400

    fix: Geo Guard list sync off by default (v1.2.5)

--- src/header.meta.js
+++ src/header.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter Experience Manager
 // @namespace    https://github.com/HyperboreanSlug/Twitter-Experience-Manager
-// @version      1.2.4
+// @version      1.2.5
 // @description  X usability toolkit: follower tracker, geo DB + tracker sync, soft region hide, mute like-notifications.
 // @author       HyperboreanSlug
 // @license      MIT
